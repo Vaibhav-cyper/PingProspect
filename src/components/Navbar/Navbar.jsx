e636fd12590ca5e24a46d5a52b1bae54a4944a0f
Vaibhav Gupta <88025368+Vaibhav-cyper@users.noreply.github.com>
2024-09-28 23:46:26 +0530

    Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 
       {/* Navigation links */}
       <ul className={`nav-links ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
-        <li ><a href="home">Home</a></li>
+        <li ><a href="/">Home</a></li>
         <li ><a href="#features">Features</a></li>
         <li><a href="#pricing">Pricing</a></li>
         <li><a href="#support">Support</a></li>
